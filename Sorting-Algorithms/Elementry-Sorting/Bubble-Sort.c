351434132e2aff31f12255ce829d2ae6698bcbfd
Abhirup Datta Khan <141467746+abhirup0199@users.noreply.github.com>
2025-04-15 17:04:39 +0000

    update

--- Sorting-Algorithms/Elementry-Sorting/Bubble-Sort.c
+++ Sorting-Algorithms/Elementry-Sorting/Bubble-Sort.c
@@ -1,40 +1,54 @@
-#include<stdio.h>
-int bubble_sort(int arr[],int n)
+#include <stdio.h>
+
+int count = 0; // Counter to track time complexity (loop operations)
+
+void bubble_sort(int arr[], int n)
 {
-    for(int i=0;i<n;i++)
+    for(int i = 0; i < n - 1; i++)
     {
-        int flag=0;
-        for(int j=0;j<n-1;j++)
+        int flag = 0;
+        count++; // Outer loop count
+        for(int j = 0; j < n - i - 1; j++)
         {
-            if(arr[j]>arr[j+1])
+            count++; // Inner loop count
+            if(arr[j] > arr[j + 1])
             {
-                int tmp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=tmp;
-                flag=1;
+                int tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+                flag = 1;
+                count++; // Swap operation count
             }
         }
-        if(flag==0)
+        if(flag == 0)
             break;
     }
-    printf("The sorted array is ----\n");
-    for(int i=0;i<n;i++)
+
+    printf("\nThe sorted array is ----\n");
+    for(int i = 0; i < n; i++)
     {
-        printf("%d\t",arr[i]);
+        printf("%d\t", arr[i]);
     }
-    return 0;
+    printf("\n");
+
+    printf("\nTotal count (approximate operations): %d\n", count);
 }
-void main()
+
+int main()
 {
     int n;
-    printf("Enter the size of the array:- ");
-    scanf("%d",&n);
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
     int array[n];
-    printf("Enter the elements of the array in the following index :-\n");
-    for(int i=0;i<n;i++)
+    printf("Enter the elements of the array:\n");
+    for(int i = 0; i < n; i++)
     {
-        printf("Array[%d] = ",i);
-        scanf("%d",&array[i]);
+        printf("Array[%d] = ", i);
+        scanf("%d", &array[i]);
     }
-    bubble_sort(array,n);
+
+    bubble_sort(array, n);
+
+    return 0;
 }
